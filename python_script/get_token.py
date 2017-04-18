7d27c40961fa80525d73942742fbe4ff8930be9a
Dai Dang Van <daidv@vn.fujitsu.com>
2017-04-18 11:51:56 +0700

    Add missing get token file

---
+++ python_script/get_token.py
@@ -0,0 +1,15 @@
+from keystoneauth1.identity import v3
+from keystoneauth1 import session
+
+from config import *
+
+
+auth = v3.Password(auth_url=AUTH_URL,
+                    user_domain_name='default',
+                    username=USERNAME,
+                    password=PASSWORD,
+                    project_domain_name='default',
+                    project_name=PROJECT_NAME)
+
+session = session.Session(auth=auth)
+TOKEN = session.get_token()
