322636d0c5f8231fd62c42fc75d17f3c6f191d07
Dai Dang Van <daidv@vn.fujitsu.com>
2017-04-24 19:00:59 +0700

    Fix upgrade command

--- upgrade_script/upgrade/keystone.sh
+++ upgrade_script/upgrade/keystone.sh
@@ -1,6 +1,7 @@
 # Start upgrade kestone
 echo "Start to upgrade Keystone on controller1"
-ssh controller1 "bash /root/test/keystone/1_upgrade_source.sh && bash /root/test/keystone/2_upgrade_database.sh"
+ssh controller1 "bash /root/test/keystone/1_upgrade_source.sh"
+ssh controller1 "bash /root/test/keystone/2_upgrade_database.sh"
 echo "Start to upgrade Keystone on controller2"
 ssh controller2 "bash /root/test/keystone/1_upgrade.sh"
 echo "Start to upgrade Keystone on controller3"
