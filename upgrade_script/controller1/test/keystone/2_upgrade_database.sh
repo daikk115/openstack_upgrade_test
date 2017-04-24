fe5c6484a473339ef6a8938fc85e6c0226fb0dac
Dai Dang Van <daidv@vn.fujitsu.com>
2017-04-24 18:49:43 +0700

    Missing enable

--- upgrade_script/controller1/test/keystone/2_upgrade_database.sh
+++ upgrade_script/controller1/test/keystone/2_upgrade_database.sh
@@ -7,5 +7,6 @@ keystone-manage db_sync --migrate
 service apache2 start
 
 dir_path=$(dirname $0)
-bash $dir_path/../haproxy/disable_server.sh keystone_admin controller1
-bash $dir_path/../haproxy/disable_server.sh keystone_api controller1
+bash $dir_path/../haproxy/enable_server.sh keystone_admin controller1
+bash $dir_path/../haproxy/enable_server.sh keystone_api controller1
+sleep 5
