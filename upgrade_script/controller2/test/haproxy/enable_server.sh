04980d90d2ff8c3af7112e7561c91c93c125af3f
Dai Dang Van <daidv@vn.fujitsu.com>
2017-04-24 18:04:16 +0700

    Disable/enable resource

--- upgrade_script/controller2/test/haproxy/enable_server.sh
+++ upgrade_script/controller2/test/haproxy/enable_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-haproxy_node = crm_mon -1 | grep haproxy | awk '{print $4}'
-echo "enable server $1/$2" | socat stdio /etc/haproxy/haproxy.sock
+haproxy_node=$(crm_mon -1 | grep haproxy | awk '{print $4}')
+echo "enable server $1/$2" | socat stdio /var/lib/haproxy/stats
+
