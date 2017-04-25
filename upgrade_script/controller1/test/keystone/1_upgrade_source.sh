31e552233ea96e7c08aebfa4d78c63e2b0823a00
Dai Dang Van <daidv@vn.fujitsu.com>
2017-04-25 09:05:00 +0700

    Update keystone

--- upgrade_script/controller1/test/keystone/1_upgrade_source.sh
+++ upgrade_script/controller1/test/keystone/1_upgrade_source.sh
@@ -4,6 +4,8 @@ dir_path=$(dirname $0)
 bash $dir_path/../haproxy/disable_server.sh keystone_admin controller1
 bash $dir_path/../haproxy/disable_server.sh keystone_api controller1
 
+sleep 10
+
 service apache2 stop
 apt-get -o Dpkg::Options::="--force-confold" -y install --only-upgrade keystone
 keystone-manage doctor
