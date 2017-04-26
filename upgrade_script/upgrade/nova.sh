bebb70accb1111fa75e098042e8e4457a759cf2d
Nam Nguyen Hoai <namnh@vn.fujitsu.com>
2017-04-26 16:41:29 +0700

    update

--- upgrade_script/upgrade/nova.sh
+++ upgrade_script/upgrade/nova.sh
@@ -25,28 +25,28 @@ ssh controller2 "bash /root/test/nova/7_nova_api_start" &
 ssh controller3 "bash /root/test/nova/7_nova_api_start" &
 wait
 sleep 3 
-echo "Starting upgrade on Compute node" &
-ssh compute1 "bash /root/test/nova/nova_compute_restart" &
-ssh compute2 "bash /root/test/nova/nova_compute_restart" &
-ssh compute3 "bash /root/test/nova/nova_compute_restart" &
-wait
-ssh controller1 "bash /root/test/nova/1_nova_api_stop" & 
-ssh controller2 "bash /root/test/nova/1_nova_api_stop" & 
-ssh controller3 "bash /root/test/nova/1_nova_api_stop" & 
-wait
-ssh controller1 "bash /root/test/nova/2_nova_service_stop" &
-ssh controller2 "bash /root/test/nova/2_nova_service_stop" &
-ssh controller3 "bash /root/test/nova/2_nova_service_stop" &
-wait
-
-ssh controller1 "bash /root/test/nova/5_nova_conductor_start" &
-ssh controller2 "bash /root/test/nova/5_nova_conductor_start" &
-ssh controller3 "bash /root/test/nova/5_nova_conductor_start" &
-wait
-ssh controller1 "bash /root/test/nova/6_nova_services_start" &
-ssh controller2 "bash /root/test/nova/6_nova_services_start" &
-ssh controller3 "bash /root/test/nova/6_nova_services_start" &
-wait
-ssh controller1 "bash /root/test/nova/7_nova_api_start" &
-ssh controller2 "bash /root/test/nova/7_nova_api_start" &
-ssh controller3 "bash /root/test/nova/7_nova_api_start" &
+#echo "Starting upgrade on Compute node" &
+#ssh compute1 "bash /root/test/nova/nova_compute_restart" &
+#ssh compute2 "bash /root/test/nova/nova_compute_restart" &
+#ssh compute3 "bash /root/test/nova/nova_compute_restart" &
+#wait
+#ssh controller1 "bash /root/test/nova/1_nova_api_stop" &
+#ssh controller2 "bash /root/test/nova/1_nova_api_stop" &
+#ssh controller3 "bash /root/test/nova/1_nova_api_stop" &
+#wait
+#ssh controller1 "bash /root/test/nova/2_nova_service_stop" &
+#ssh controller2 "bash /root/test/nova/2_nova_service_stop" &
+#ssh controller3 "bash /root/test/nova/2_nova_service_stop" &
+#wait
+#
+#ssh controller1 "bash /root/test/nova/5_nova_conductor_start" &
+#ssh controller2 "bash /root/test/nova/5_nova_conductor_start" &
+#ssh controller3 "bash /root/test/nova/5_nova_conductor_start" &
+#wait
+#ssh controller1 "bash /root/test/nova/6_nova_services_start" &
+#ssh controller2 "bash /root/test/nova/6_nova_services_start" &
+#ssh controller3 "bash /root/test/nova/6_nova_services_start" &
+#wait
+#ssh controller1 "bash /root/test/nova/7_nova_api_start" &
+#ssh controller2 "bash /root/test/nova/7_nova_api_start" &
+#ssh controller3 "bash /root/test/nova/7_nova_api_start" &
