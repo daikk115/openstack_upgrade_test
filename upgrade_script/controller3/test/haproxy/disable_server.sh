c95aaeebcc170c066c721e750bd970278031d9bc
Nam Nguyen Hoai <namnh@vn.fujitsu.com>
2017-04-26 11:32:42 +0700

    update

--- upgrade_script/controller3/test/haproxy/disable_server.sh
+++ upgrade_script/controller3/test/haproxy/disable_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-haproxy_node=$(crm_mon -1 | grep haproxy | awk '{print $4}')
+haproxy_node=$(crm_mon -1 | grep "lsb:haproxy" | awk '{print $4}')
 ssh $haproxy_node "echo 'disable server $1/$2' | socat stdio /var/lib/haproxy/stats"
 sleep 5
