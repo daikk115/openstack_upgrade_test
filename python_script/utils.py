86796c67ad8e26cda7fe9e13fbcff0808deecf57
Dai Dang Van <daidv@vn.fujitsu.com>
2017-04-27 09:58:48 +0700

    update

--- python_script/utils.py
+++ python_script/utils.py
@@ -20,13 +20,14 @@ def bg_cb(sess, resp):
     :return:
     """
 
-    timestamp = time.time() * 1000
-    tasks.append({
-        "timestamp": timestamp,
-        "status": resp.status_code
-    })
-    print("%d - %d - %s" % (timestamp, resp.status_code, resp.request.method))
-    print("via: {}" .format(resp.url))
+    if continue_test:
+        timestamp = time.time() * 1000
+        tasks.append({
+            "timestamp": timestamp,
+            "status": resp.status_code
+        })
+        print("%d - %d - %s" % (timestamp, resp.status_code, resp.request.method))
+        print("via: {}" .format(resp.url))
 
 
 def error_request_counter():
@@ -45,17 +46,17 @@ def error_request_counter():
             if (int(task.get('status')) / 100) == 5:
                 count += 1
                 is_find_start = False
-                error_dict[task.get('status')] = 1
         else:
             if (int(task.get('status')) / 100) == 5:
                 count += 1
-            try:
-                error_dict[task.get('status')] += 1
-            except:
-                error_dict[task.get('status')] = 1
+        try:
+            error_dict[task.get('status')] += 1
+        except:
+            error_dict[task.get('status')] = 1
 
     print("Number of fail requests (status code >= 500): {}".format(count))
     print(error_dict)
+
 
 def downtime_counter():
     """ Return downtime of testing process
@@ -75,7 +76,6 @@ def downtime_counter():
                 count += 1
                 is_find_start = False
                 start = task.get('timestamp')
-                error_dict[task.get('status')] = 1
         else:
             if (int(task.get('status')) / 100) == 5:
                 count += 1
@@ -83,14 +83,15 @@ def downtime_counter():
                 end = task.get('timestamp')
                 is_find_start = True
                 downtime += end - start
-            try:
-                error_dict[task.get('status')] += 1
-            except:
-                error_dict[task.get('status')] = 1
+        try:
+            error_dict[task.get('status')] += 1
+        except:
+            error_dict[task.get('status')] = 1
 
     print("Downtime for rolling upgrade process: {} ms".format(downtime))
     print("Number of fail requests (status code >= 500): {}".format(count))
     print(error_dict)
+
 
 def send_request(url, method, headers=None, data=None, **kwargs):
     if method == 'GET':
@@ -110,14 +111,12 @@ def send_request(url, method, headers=None, data=None, **kwargs):
     else:
         print("Method does not support: {}".format(method))
 
+
 def signal_handler(signal, frame):
     global continue_test
     continue_test = False
-    time.sleep(3)
-    print continue_test
-    error_request_counter()
-    global tasks
-    print("Number of requests that we sent: {}", format(len(tasks)))
-    sys.exit(1)
+    downtime_counter()
+    print("Number of requests that we sent and received result: {}", format(len(tasks)))
+    sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
