7b9c9fbf9786308f349fe67ce9cbb0b1c1b4feb6
Nam Nguyen Hoai <namnh@vn.fujitsu.com>
2017-04-27 10:07:50 +0700

    update

--- upgrade_script/upgrade/keystone.sh
+++ upgrade_script/upgrade/keystone.sh
@@ -1,4 +1,8 @@
 # Start upgrade kestone
+echo "Set global variable to Database"
+sleep 1
+mysql -uroot -pnam123 -h10.164.180.87 -e "set global log_bin_trust_function_creators=1"
+
 echo "Start to upgrade Keystone on controller1"
 ssh controller1 "bash /root/test/keystone/1_upgrade_source.sh"
 ssh controller1 "bash /root/test/keystone/2_upgrade_database.sh"
