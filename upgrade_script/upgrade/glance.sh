ce7006314624e2f7441fb74a514ff34b6d4c7aed
Nam Nguyen Hoai <namnh@vn.fujitsu.com>
2017-04-28 13:48:30 +0700

    update for glance

--- upgrade_script/upgrade/glance.sh
+++ upgrade_script/upgrade/glance.sh
@@ -1,4 +1,8 @@
 # Start upgrade kestone
+echo "Set global variable to Database"
+sleep 1
+mysql -uroot -pnam123 -h10.164.180.87 -e "set global log_bin_trust_function_creators=1"
+
 echo "Start to upgrade Glance on controller1"
 sleep 2
 ssh controller1 "bash /root/test/glance/1_upgrade_source_and_database.sh"
